6eaf4973cf91e0886c06e8dbaab99dce94697295
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-05-26 01:20:27 +0300

    simplify
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/unistack-org/micro-codec-yaml/v3
 
-go 1.15
+go 1.16
 
 require (
 	github.com/ghodss/yaml v1.0.0
-	github.com/unistack-org/micro/v3 v3.3.17
+	github.com/unistack-org/micro/v3 v3.3.19
 )
--- yaml.go
+++ yaml.go
@@ -3,35 +3,44 @@ package yaml
 
 import (
 	"io"
-	"io/ioutil"
 
 	"github.com/ghodss/yaml"
 	"github.com/unistack-org/micro/v3/codec"
+	rutil "github.com/unistack-org/micro/v3/util/reflect"
 )
 
 type yamlCodec struct{}
 
-func (c *yamlCodec) Marshal(b interface{}) ([]byte, error) {
-	switch m := b.(type) {
+const (
+	flattenTag = "flatten"
+)
+
+func (c *yamlCodec) Marshal(v interface{}) ([]byte, error) {
+	switch m := v.(type) {
 	case nil:
 		return nil, nil
 	case *codec.Frame:
 		return m.Data, nil
 	}
 
-	return yaml.Marshal(b)
+	if nv, nerr := rutil.StructFieldByTag(v, codec.DefaultTagName, flattenTag); nerr == nil {
+		v = nv
+	}
+	return yaml.Marshal(v)
 }
 
 func (c *yamlCodec) Unmarshal(b []byte, v interface{}) error {
-	if len(b) == 0 {
+	if len(b) == 0 || v == nil {
 		return nil
 	}
-	switch m := v.(type) {
-	case nil:
-		return nil
-	case *codec.Frame:
+
+	if m, ok := v.(*codec.Frame); ok {
 		m.Data = b
 		return nil
+	}
+
+	if nv, nerr := rutil.StructFieldByTag(v, codec.DefaultTagName, flattenTag); nerr == nil {
+		v = nv
 	}
 
 	return yaml.Unmarshal(b, v)
@@ -41,48 +50,34 @@ func (c *yamlCodec) ReadHeader(conn io.Reader, m *codec.Message, t codec.Message
 	return nil
 }
 
-func (c *yamlCodec) ReadBody(conn io.Reader, b interface{}) error {
-	switch m := b.(type) {
-	case nil:
-		return nil
-	case *codec.Frame:
-		buf, err := ioutil.ReadAll(conn)
-		if err != nil {
-			return err
-		} else if len(buf) == 0 {
-			return nil
-		}
-		m.Data = buf
+func (c *yamlCodec) ReadBody(conn io.Reader, v interface{}) error {
+	if v == nil {
 		return nil
 	}
 
-	buf, err := ioutil.ReadAll(conn)
+	buf, err := io.ReadAll(conn)
 	if err != nil {
 		return err
 	} else if len(buf) == 0 {
-		// not needed but similar changes in all codecs
 		return nil
 	}
 
-	return yaml.Unmarshal(buf, b)
+	return c.Unmarshal(buf, v)
 }
 
-func (c *yamlCodec) Write(conn io.Writer, m *codec.Message, b interface{}) error {
-	switch m := b.(type) {
-	case nil:
+func (c *yamlCodec) Write(conn io.Writer, m *codec.Message, v interface{}) error {
+	if v == nil {
 		return nil
-	case *codec.Frame:
-		_, err := conn.Write(m.Data)
-		return err
 	}
 
-	buf, err := yaml.Marshal(b)
+	buf, err := c.Marshal(v)
 	if err != nil {
 		return err
+	} else if len(buf) == 0 {
+		return codec.ErrInvalidMessage
 	}
 
 	_, err = conn.Write(buf)
-
 	return err
 }
 
